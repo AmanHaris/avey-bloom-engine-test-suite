3af2138a0b60cc45cdaa4af553322395cce6d52b
Aman Haris <aman.s.haris@gmail.com>
2023-10-26 11:27:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+How to use:
+
+Step 1: Activate the venv
+
+Enter into terminal:
+source ./venv/bin/activate
+
+Step 2: install any dependencies
+
+pip3 install -r requirements.txt
+
+Step 3: run run.py with necessary options
---
+++ run.py
@@ -0,0 +1,26 @@
+import os
+import json
+import pandas
+import tqdm
+
+
+# load files
+
+
+# run PLE
+
+
+# run MUE
+
+
+# run TPM
+
+
+# save files
+
+    # version = date + time
+    # mkdir for version
+    # store results in versioned dir
+
+print("hello")
+
